3a21485e4d6490a4bc3798c1b0f02750a2841d9d
Joel Heymbeeck <heymbeeck.joel@gmail.com>
2011-12-17 08:33:20 +0100

    Version 1.0  Blizzard raid frames killed. Signed-off-by: Joel Heymbeeck <heymbeeck.joel@gmail.com>

--- Tukui_Healium/init.lua
+++ Tukui_Healium/init.lua
@@ -20,3 +20,27 @@ else
 	print(L.healium_GREETING_VERSIONUNKNOWN)
 end
 print(L.healium_GREETING_OPTIONS)
+
+local killBlizzardFrames = CreateFrame("Frame")
+killBlizzardFrames:RegisterEvent("PLAYER_LOGIN")
+killBlizzardFrames:SetScript("OnEvent", function(self, event, arg1)
+	if event == "PLAYER_LOGIN" then
+		-- Kill blizzard raid frames
+		local dummy = function() return end
+		local function Kill(object)
+			if object.UnregisterAllEvents then
+				object:UnregisterAllEvents()
+			end
+			object.Show = dummy
+			object:Hide()
+		end
+		InterfaceOptionsFrameCategoriesButton10:SetScale(0.00001)
+		InterfaceOptionsFrameCategoriesButton10:SetAlpha(0)
+		InterfaceOptionsFrameCategoriesButton11:SetScale(0.00001)
+		InterfaceOptionsFrameCategoriesButton11:SetAlpha(0)
+		Kill(CompactRaidFrameManager)
+		Kill(CompactRaidFrameContainer)
+		CompactUnitFrame_UpateVisible = dummy
+		CompactUnitFrame_UpdateAll = dummy
+	end
+end)
